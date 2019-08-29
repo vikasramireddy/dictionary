00798c2224e137629589432f725f231a96083c13
Vikas Ramireddy <vikasramireddy@gmail.com>
2019-08-29 18:44:12 +0530

    Added Dictionary API

---
+++ src/main/java/com/techgig/dictionary/sampledictionary/DictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.techgig.dictionary.sampledictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryApplication.class, args);
+	}
+
+}
